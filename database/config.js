cb73896229549479670f91ee39aaa6f5b7db6843
Musa_Fadul <32779553+MusaFadul@users.noreply.github.com>
2019-05-08 09:51:20 +0200

    Update config.js

--- database/config.js
+++ database/config.js
@@ -1,9 +1,9 @@
 const config = {
-    databaseName : 'd5hmj3uphkgthp',
-    databaseHost : 'ec2-54-225-242-183.compute-1.amazonaws.com',
-    databaseUserName : 'mktjjgseossptm',
+    databaseName : '',
+    databaseHost : '',
+    databaseUserName : 'm',
     databasePort:'5432',
-    databasePassword : 'cc66efde430f1bf072320e9ba7e28fffb60f7594f0401abe11bf410afbfcb50d',
+    databasePassword : '',
     databaseDialect:'postgres'
 }
 
